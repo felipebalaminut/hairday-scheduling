b04aa2a618137fc132f7c17bb2f78ef2aea68227
Felipe Fonseca <felipebalaminutdev@gmail.com>
2025-08-26 13:52:16 -0300

    fix: resolve build error by adding babel and correcting syntax on package.json file

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        teste: /\.js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
